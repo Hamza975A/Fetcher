d5b2cd6daa75e66cb49f08c88c3bde1b9d98509b
Holden Gutwin (hcg369) <hcg369@mail.usask.ca>
2022-03-02 12:25:26 -0600

    Added orders list page

---
+++ frontend/pages/orders-list/index.js
@@ -0,0 +1,51 @@
+import React from "react";
+
+import { Layout } from "../../components/Layout";
+import { OrderCard } from "../../components/OrdersList";
+
+/**
+ * Home page for the website.
+ * @return {JSX.Element}
+ */
+export default function Home() {
+  return (
+    <Layout>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+
+      <h2>Current Orders</h2>
+
+      <OrderCard
+        ordernum="123456789"
+        date="Thu, Feb. 10"
+        price="60.50"
+        buttontext="VIEW ORDER"
+      />
+
+      <br></br>
+
+      <OrderCard
+        ordernum="66666666"
+        date="Tue, Jan. 9"
+        price="44.44"
+        buttontext="VIEW ORDER"
+      />
+
+      <br></br>
+      <br></br>
+
+      <h2>Past Orders</h2>
+
+      <OrderCard
+        ordernum="987654321"
+        date="Wed, Mar. 2"
+        price="20.30"
+        buttontext="SHOW RECEIPT"
+      />
+    </Layout>
+  );
+}
