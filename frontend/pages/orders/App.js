77874c14b7b91f08c58e5c65d5e7f6d4f1d1726b
llh430 <llh430@usask.ca>
2022-03-09 11:31:10 -0600

    fixed bug

--- frontend/pages/orders/App.js
+++ frontend/pages/orders/App.js
@@ -30,6 +30,27 @@ function App() {
     items = JSON.parse(localStorage.getItem("placeOrder"));
     extraDetails = JSON.parse(localStorage.getItem("extraDetails"));
     cart = JSON.parse(localStorage.getItem("checkout"));
+    if(items==null){
+      items=[{ id: 0, Size: "", Address: "", Details: "", ImportantDetails: "" },]
+    }
+    if(extraDetails==null){
+      extraDetails=[{ postID: 0, prevAddress: "", startTime: "7:00 AM", endTime: "8:00 PM" }]
+    }
+    if(cart==null){
+       cart = [
+        {
+          dropoffLocation: "anywhere",
+          priority: 1,
+          instructions: "",
+          cost: "",
+          email: "",
+          number: "",
+          expirationDate: "",
+          cvc: "",
+          cardName: "",
+          postal: "",
+        },]}
+    
   }
 
   const [packages, setPackages] = useState(items);
