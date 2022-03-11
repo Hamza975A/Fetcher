1d2817a3a1a9fb8c965fe07fd197e67c27eb485a
Tanmay Pathak <71184799+tanmay-pathak@users.noreply.github.com>
2022-03-10 23:03:05 -0600

    bug fix: destination address wasnt added as a mark

--- frontend/components/Maps.js
+++ frontend/components/Maps.js
@@ -2,16 +2,24 @@ import React from "react";
 import mapStyles from "../styles/mapStyles";
 import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
 import { getFromStorage } from "../lib/storage-tools";
+
+// google maps libraries to be enabled
 const libraries = ["places"];
+
+// map container style options
 const mapContainerStyle = {
   width: "60%",
   boxShadow: "0.05rem 0.1rem 0.3rem -0.03rem rgba(0, 0, 0, 0.45)",
   height: "50vh",
 };
+
+// default map center
 const center = {
   lat: 52.125058,
   lng: -106.650718,
 };
+
+// extra map options
 const options = {
   styles: mapStyles,
   disableDefaultUI: true,
@@ -20,23 +28,27 @@ const options = {
 
 /**
  * Function to render a google map component with markers placed on it.
- * @param {markers} : Destination address
+ * Other markers are fetched from the local storage.
+ * @param {markers} : Destination address object
  * @return {JSXComponent} : Map component rendered with markers for different addresses
  */
 const Maps = ({ markers }) => {
+  // load and configure google maps component from @react-google-maps/api
   const { isLoaded, loadError } = useLoadScript({
     googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
     libraries,
   });
   if (loadError) return "Error loading maps";
   if (!isLoaded) return "Loading Maps";
-  const marks = [];
 
+  // list of {lat, lng} for markers | initial marker is the destination address
+  const marks = [markers.geometry.location];
+
+  // fetch other addresses from local storage
   const object = getFromStorage("placeOrder");
   object.map((o) => {
     marks.push(o.Address.geometry.location);
   });
-  // array of {lat: "", lng: ""} items to be used as markers
 
   return (
     <GoogleMap
@@ -45,6 +57,7 @@ const Maps = ({ markers }) => {
       center={center}
       options={options}
     >
+      {/* place each marker on the map */}
       {marks.map((mark, index) => (
         <Marker key={index} position={mark} />
       ))}
